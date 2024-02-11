6f4787eeffc9d5dae487c2d95396ae42d6877ded
Wojciech Jozwik <wj.telefon@gmail.com>
2024-02-12 00:59:25 +0100

    Add basic components

---
+++ src/components/Button/Button.js
@@ -0,0 +1,10 @@
+import styles from './Button.module.scss';
+
+const Button = props => {
+    return <button className={styles.button}>
+    {props.children}
+    </button>
+};
+
+
+export default Button;
---
+++ src/components/List/List.js
@@ -0,0 +1,20 @@
+import styles from './List.module.scss';
+import Column from '../Column/Column';
+
+const List = () => {
+  return (
+    <div className={styles.list}>
+      <header className={styles.header}>
+        <h2 className={styles.title}>Things to do<span>soon!</span></h2>
+      </header>
+      <p className={styles.description}>Interesting things I want to check out</p>
+      <section className={styles.columns}>
+        <Column icon='book' title='Books'/>
+        <Column icon='film' title='Movies'/>
+        <Column icon='gamepad' title='Games'/>
+      </section>
+    </div>
+  );
+};
+
+export default List;
